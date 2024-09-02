861195227c948a048950d3f0f5245b686b1c6dc0
Allan Diniz Vieira <allandvieira@hotmail.com>
2024-09-02 16:09:33 -0300

    Pós Testes Integração

---
+++ LocadoraDeVeiculos.Testes.Integracao/ModuloGrupoVeiculos/RepositorioGrupoVeiculosEmOrmTests.cs
@@ -0,0 +1,72 @@
+﻿using FizzWare.NBuilder;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infra.Orm.ModuloGrupoVeiculos;
+
+namespace LocadoraDeVeiculos.Testes.Integracao.ModuloGrupoVeiculos;
+
+[TestClass]
+[TestCategory("Integração")]
+public class RepositorioGrupoVeiculosEmOrmTests
+{
+    private LocadoraDbContext dbContext;
+    private RepositorioGrupoVeiculosEmOrm repositorio;
+
+    [TestInitialize]
+    public void Inicializar()
+    {
+        dbContext = new LocadoraDbContext();
+
+        dbContext.GruposVeiculos.RemoveRange(dbContext.GruposVeiculos);
+
+        repositorio = new RepositorioGrupoVeiculosEmOrm(dbContext);
+
+        BuilderSetup.SetCreatePersistenceMethod<GrupoVeiculos>(repositorio.Inserir);
+    }
+
+    [TestMethod]
+    public void Deve_Inserir_GrupoVeiculos()
+    {
+        var grupo = Builder<GrupoVeiculos>
+            .CreateNew()
+            .Persist();
+
+        var grupoSelecionado = repositorio.SelecionarPorId(grupo.Id);
+
+        Assert.IsNotNull(grupoSelecionado);
+        Assert.AreEqual(grupo, grupoSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_GrupoVeiculos()
+    {
+        var grupo = Builder<GrupoVeiculos>
+            .CreateNew()
+            .Persist();
+
+        grupo.Nome = "Teste de Edição";
+        repositorio.Editar(grupo);
+
+        var grupoSelecionado = repositorio.SelecionarPorId(grupo.Id);
+
+        Assert.IsNotNull(grupoSelecionado);
+        Assert.AreEqual(grupo, grupoSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_GrupoVeiculos()
+    {
+        var grupo = Builder<GrupoVeiculos>
+            .CreateNew()
+            .Persist();
+
+        repositorio.Excluir(grupo);
+
+        var grupoSelecionado = repositorio.SelecionarPorId(grupo.Id);
+
+        var grupos = repositorio.SelecionarTodos();
+
+        Assert.IsNull(grupoSelecionado);
+        Assert.AreEqual(0, grupos.Count);
+    }
+}
