5b2af2605a3b9834f1d1f387e8c99d07060c4c15
Allan Diniz Vieira <allandvieira@hotmail.com>
2024-09-03 03:01:56 -0300

    Finalizando veículos

--- LocadoraDeVeiculos.WebApp/Mapping/VeiculoProfile.cs
+++ LocadoraDeVeiculos.WebApp/Mapping/VeiculoProfile.cs
@@ -1,5 +1,28 @@
-﻿namespace LocadoraDeVeiculos.WebApp.Mapping;
+﻿using AutoMapper;
+using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
+using LocadoraDeVeiculos.WebApp.Models;
 
-public class VeiculoProfile
+namespace LocadoraDeVeiculos.WebApp.Mapping;
+
+public class VeiculoProfile : Profile
 {
+    public VeiculoProfile()
+    {
+        CreateMap<InserirVeiculoViewModel, Veiculo>();
+        CreateMap<EditarVeiculoViewModel, Veiculo>();
+
+        CreateMap<Veiculo, ListarVeiculoViewModel>()
+            .ForMember(
+                dest => dest.GrupoVeiculos,
+                opt => opt.MapFrom(src => src.GrupoVeiculos!.Nome)
+            );
+
+        CreateMap<Veiculo, DetalhesVeiculoViewModel>()
+            .ForMember(
+                dest => dest.GrupoVeiculos,
+                opt => opt.MapFrom(src => src.GrupoVeiculos!.Nome)
+            );
+
+        CreateMap<Veiculo, EditarVeiculoViewModel>();
+    }
 }
