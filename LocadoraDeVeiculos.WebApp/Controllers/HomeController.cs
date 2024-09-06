1586d65a19ae3653bf7b3674f9c62ddf6ddd54d4
Allan Diniz Vieira <allandvieira@hotmail.com>
2024-09-06 16:06:20 -0300

    Validando master

--- LocadoraDeVeiculos.WebApp/Controllers/HomeController.cs
+++ LocadoraDeVeiculos.WebApp/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LocadoraDeVeiculos.WebApp.Controllers;
 
+[Authorize(Roles = "Empresa,Funcionario")]
 public class HomeController : Controller
 {
     public IActionResult Index()
